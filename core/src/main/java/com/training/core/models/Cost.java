72f2fbc872791cf44e90cf04b20f11f7cc825738
vedapand <vpga117@gmail.com>
2023-05-03 11:27:15 +0530

    first commit

---
+++ core/src/main/java/com/training/core/models/Cost.java
@@ -0,0 +1,11 @@
+package com.training.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class)
+public class Cost {
+
+
+
+}
